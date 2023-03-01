c962c5795203078bd9a9b2bbb197ca534834b877
yukki96 <zyadman01@hotmail.fr>
2023-03-01 13:25:29 -0500

    commit

--- routes.py
+++ routes.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request, g
+from .database import Database
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
 
 @app.route('/')
 def index(): 
     title= "Publications"
-    return render_template('login.html', title=title)
+    return render_template('accueil.html', title=title)
 
 @app.route('/form', methods=["POST"])
 def form():
