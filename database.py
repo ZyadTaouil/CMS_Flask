ac719d774bccbeb040b05921f3fef1e1ce376237
yukki96 <zyadman01@hotmail.fr>
2023-03-02 10:40:06 -0500

    commit

--- database.py
+++ database.py
@@ -4,8 +4,6 @@ class Database:
     def __init__(self):
         self.connection = None
 
-    titre, identifiant, auteur, date_publication, paragraphe = row
-
     def get_connection(self):
         if self.connection is None:
             self.connection = sqlite3.connect('db/article.db')
@@ -28,9 +26,59 @@ class Database:
         auteurs = cursor.fetchall()
         return [auteur[0] for auteur in auteurs]
 
-    def insert_article(titre, identifiant, auteur, date_publication, paragraphe):
+    def get_article_by_id(self, id):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select * from article where identifiant = ? ", (id,))
+        article = cursor.fetchone() 
+        return article
+
+    def get_all_articles(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select * from article")
+        articles = cursor.fetchall() 
+        return articles
+
+    def get_all_articles_id(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select identifiant from article")
+        articles = cursor.fetchall()
+        new_articles = [article[0] for article in articles] 
+        return new_articles
+
+
+    def get_last_five_posts(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("select * from article Where date_publication = DATE('now','localtime') ORDER BY date_publication DESC LIMIT 5")
+        publications = cursor.fetchall()
+        return publications
+
+    def delete_insertions(self):
+        cursor = self.get_connection().cursor()
+        cursor.execute("delete * from article")
+
+    def search(self, query):
+        cursor = self.get_connection().cursor()
+        # Sélectionne les publications contenant la chaîne de recherche
+        cursor.execute("SELECT titre, date_publication, identifiant FROM article WHERE (titre LIKE ? OR paragraphe LIKE ?) AND date_publication <= date('now')", 
+            ('%' + query + '%', '%' + query + '%'))
+        resultats = cursor.fetchall()
+        return resultats
+
+    def insert_article(self, titre, identifiant, auteur, date_publication, paragraphe):
         connection = self.get_connection()
         cursor = connection.cursor()
         cursor.execute(("insert into article(titre, identifiant, auteur, date_publication, paragraphe)"
                         "values(?, ?, ?, ?, ?)"), (titre, identifiant, auteur, date_publication, paragraphe))
+        connection.commit()
+
+    def update_title(self,id,title):
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("update article SET titre = ? WHERE identifiant=?", (title,id))
+        connection.commit()
+
+    def update_content(self,id,content):
+        connection = self.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("update article SET paragraphe = ? WHERE identifiant=?", (content,id))
         connection.commit()
--- routes.py
+++ routes.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, g
+from flask import Flask
+from flask import g
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
 from .database import Database
 
 app = Flask(__name__)
@@ -17,12 +21,66 @@ def close_connection(exception):
 
 @app.route('/')
 def index(): 
-    title= "Publications"
-    return render_template('accueil.html', title=title)
+    posts = get_db().get_last_five_posts()
+    return render_template('accueil.html', title="Publications", posts=posts)
 
-@app.route('/form', methods=["POST"])
-def form():
-    age = request.form['age']
-    genre = request.form['genre']
-    nom = request.form['nom']
-    return render_template('form.html',title="Merci", age=age, genre=genre, nom=nom)
+# Route pour la recherche
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    results = get_db().search(query)
+    return render_template('search.html', query=query, results=results)
+
+
+@app.route('/article/<identifiant>')
+def article(identifiant):
+    article = get_db().get_article_by_id(identifiant) 
+    if not article:
+        return render_template('404.html'), 404
+    return render_template('article.html', article=article)
+
+@app.route('/admin')
+def admin():
+    articles = get_db().get_all_articles()
+    return render_template('admin.html', articles=articles)
+
+@app.route('/admin/modifier/<identifiant>', methods=['GET', 'POST'])
+def modifier(identifiant):
+    identifiants = get_db().get_all_articles_id()
+    if identifiant not in identifiants:
+        return render_template('404.html'), 404
+
+    article = get_db().get_article_by_id(identifiant)
+    if request.method == 'POST':
+        new_titre = request.form.get('titre')
+        new_paragraphe = request.form.get('paragraphe')
+
+        get_db().update_title(new_titre,identifiant)
+        get_db().update_content(new_paragraphe,identifiant)
+
+        # Rediriger l'utilisateur vers la page d'administration après modification
+        return redirect('/admin')
+
+    # Rendre le template HTML en passant l'article à modifier
+    return render_template('modification.html', article=article)
+
+@app.route('/admin-nouveau', methods=['GET', 'POST'])
+def creer_article():
+    if request.method == 'POST':
+        titre = request.form.get('titre')
+        paragraphe = request.form.get('paragraphe')
+        date_publication = datetime.datetime.now()
+        
+        nouvel_article = Article(titre=titre, paragraphe=paragraphe, date_publication=date_publication)
+        get_db().session.add(nouvel_article)
+        get_db().session.commit()
+        
+        # Rediriger vers la page d'accueil
+        return redirect('/')
+    
+    # Si la requête est de type GET, afficher le formulaire de création d'article
+    return render_template('creation.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
